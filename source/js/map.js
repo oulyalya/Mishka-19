ceab1e6dff80d9db8d576f40b839ab42ef6855c8
Gelezka20160321\oulyalya <olya.debelaya@gmail.com>
2020-05-22 22:16:58 +0300

    Local fonts

--- source/js/map.js
+++ source/js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
   var opt = {
     center: {lat: 59.938792, lng: 30.319416},
-    zoom: 15
+    zoom: 16
   }
 
   var map = new google.maps.Map(document.getElementById("map"), opt);
@@ -9,6 +9,6 @@ function initMap() {
   var marker = new google.maps.Marker({
       position: {lat: 59.938829, lng: 30.323053},
       map: map,
-      // icon: '../img/map-pin.png'
+      icon: '../img/icon-map-pin.svg'
     });
 }
